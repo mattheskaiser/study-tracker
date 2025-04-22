eadd92a447be98bc5c5896ba2e536da6c25d9d85
Matthes Kaiser <mattheskaiser02@gmail.com>
2025-04-21 20:04:46 -0700

    feat(courses): implement button to toggle courses

--- src/components/atoms/Button.atom.tsx
+++ src/components/atoms/Button.atom.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import type { ReactNode } from "react";
+import type { ClassValue } from "clsx";
 
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 type ButtonAtomProps = {
   children?: string;
@@ -13,6 +15,7 @@ type ButtonAtomProps = {
   endContent?: ReactNode;
   type?: "button" | "submit" | "reset";
   disabled?: boolean;
+  className?: ClassValue;
 };
 
 export const ButtonAtom = ({
@@ -24,9 +27,15 @@ export const ButtonAtom = ({
   endContent,
   type,
   disabled,
+  className,
 }: ButtonAtomProps) => {
   return isIconOnly ? (
-    <Button type={type} disabled={disabled} onClick={onPress}>
+    <Button
+      type={type}
+      disabled={disabled}
+      onClick={onPress}
+      className={cn("", className)}
+    >
       {icon}
     </Button>
   ) : (
@@ -34,7 +43,10 @@ export const ButtonAtom = ({
       disabled={disabled}
       type={type}
       onClick={onPress}
-      className="flex flex-row items-center justify-center text-base hover:cursor-pointer"
+      className={cn(
+        "flex flex-row items-center justify-center text-base hover:cursor-pointer",
+        className,
+      )}
     >
       {startContent}
       {children}
--- src/components/organisms/CourseList.organism.tsx
+++ src/components/organisms/CourseList.organism.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
+import { ArrowDown, ArrowUp } from "lucide-react";
 import { useQueryState } from "nuqs";
 
+import { ButtonAtom } from "@/components/atoms/Button.atom";
 import { TextAtom } from "@/components/atoms/Text.atom";
 import { LoadingSpinnerMolecule } from "@/components/molecules/LoadingSpinner.molecule";
 import { CourseTabOrganism } from "@/components/organisms/CourseTab.organism";
@@ -11,6 +13,7 @@ export const CourseListOrganism = () => {
   const [courses, setCourses] = useState<CourseType[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const [showAllCourses, setShowAllCourses] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchCourses = async () => {
@@ -68,15 +71,36 @@ export const CourseListOrganism = () => {
   }
   return (
     <div className="flex flex-col gap-y-2">
-      {courses.map((course) => (
-        <CourseTabOrganism
-          key={course.id}
-          id={course.id}
-          name={course.name}
-          status={course.status}
-          grade={course.grade}
-        />
-      ))}
+      {showAllCourses
+        ? courses.map((course) => (
+            <CourseTabOrganism
+              key={course.id}
+              id={course.id}
+              name={course.name}
+              status={course.status}
+              grade={course.grade}
+            />
+          ))
+        : courses
+            .slice(0, 5)
+            .map((course) => (
+              <CourseTabOrganism
+                key={course.id}
+                id={course.id}
+                name={course.name}
+                status={course.status}
+                grade={course.grade}
+              />
+            ))}
+      {courses.length > 5 && (
+        <ButtonAtom
+          className="mt-6"
+          endContent={showAllCourses ? <ArrowUp /> : <ArrowDown />}
+          onPress={() => setShowAllCourses((prev) => !prev)}
+        >
+          {showAllCourses ? "Kurse einklappen" : "Alle Kurse anzeigen"}
+        </ButtonAtom>
+      )}
     </div>
   );
 };
