e62a49fd3175a0cc3117bd0591dd605bc9705b09
Matthes Kaiser <mattheskaiser02@gmail.com>
2025-05-21 13:43:20 -0700

    feat(translation): create custom hook to handle logic for choosing correct translation file

---
+++ src/hooks/useTranslation.hook.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { useContext, useEffect, useState } from "react";
+
+import { LanguageContext } from "@/context";
+import { deTranslations } from "@/translations/de.translations";
+import { enTranslations } from "@/translations/en.translations";
+import { espTranslations } from "@/translations/esp.translations";
+
+export const useTranslation = () => {
+  const { language } = useContext(LanguageContext);
+  const [translationFile, setTranslationFile] = useState(enTranslations);
+
+  useEffect(() => {
+    if (language) {
+      if (language === "en") setTranslationFile(enTranslations);
+      if (language === "esp") setTranslationFile(espTranslations);
+      if (language === "de") setTranslationFile(deTranslations);
+    }
+  }, [language]);
+
+  return translationFile;
+};
