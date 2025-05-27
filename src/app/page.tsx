4a3b7710a366d827d59a0c2fde53cf5c56166867
Matthes Kaiser <mattheskaiser02@gmail.com>
2025-05-27 16:48:56 -0700

    feat(tanstack): install and set up tanstack query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,25 @@
 "use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { AccountFinderCardOrganism } from "@/components/organisms/AccountFinderCard.organism";
 import { CourseManagerCardOrganism } from "@/components/organisms/CourseManagerCard.organism";
 import { OverviewCardOrganism } from "@/components/organisms/OverviewCard.organism";
 
+const queryClient = new QueryClient();
+
 const Home = () => {
   return (
-    <div className="no-scrollbar container mx-auto min-h-screen w-full overflow-x-hidden overflow-y-scroll">
-      <div className="flex flex-col gap-y-6 px-6">
-        <AccountFinderCardOrganism />
-        <div className="flex flex-col gap-6 lg:flex-row">
-          <CourseManagerCardOrganism />
-          <OverviewCardOrganism />
+    <QueryClientProvider client={queryClient}>
+      <div className="no-scrollbar container mx-auto min-h-screen w-full overflow-x-hidden overflow-y-scroll">
+        <div className="flex flex-col gap-y-6 px-6">
+          <AccountFinderCardOrganism />
+          <div className="flex flex-col gap-6 lg:flex-row">
+            <CourseManagerCardOrganism />
+            <OverviewCardOrganism />
+          </div>
         </div>
       </div>
-    </div>
+    </QueryClientProvider>
   );
 };
 
